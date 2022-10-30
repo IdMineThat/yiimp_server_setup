1ded6b01c06b0bf87440495182822b834cf50b17
IdMineThat <100437916+IdMineThat@users.noreply.github.com>
2022-10-29 20:30:08 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 ################################################################################
 # Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox #
-# Updated by Dirty Harry for YiiMP use...                                      #
+# Updated by IdMineThat for YiiMP use...                                      #
 # This script is intended to be ran from the Yiimp Server Installer            #
 ################################################################################
 
@@ -58,8 +58,8 @@ if [[ ("$FIRST_TIME_SETUP" == "1") ]]; then
   # Are we running as root?
   if [[ $EUID -ne 0 ]]; then
     # Welcome
-    message_box "Dirty Harry Yiimp Server Setup Installer v1.0" \
-    "Hello and thanks for using the Dirty Harry Yiimp Setup Installer!
+    message_box "IdMineThat Yiimp Server Setup Installer v1.0" \
+    "Hello and thanks for using the IdMineThat Yiimp Setup Installer!
     \n\nInstallation for the most part is fully automated. In most cases any user responses that are needed are asked prior to the installation.
     \n\nNOTE: You should only install this on a brand new Ubuntu 16.04 or Ubuntu 18.04 installation."
   	source existing_user.sh
@@ -98,7 +98,7 @@ clear
 	echo
 	echo "-----------------------------------------------"
 	echo
-	echo Thank you for using the Dirty Harry Yiimp Server Installer!
+	echo Thank you for using the IdMineThat Yiimp Server Installer!
 	echo
 	echo To run this installer anytime simply type, yiimpserver!
 	echo
