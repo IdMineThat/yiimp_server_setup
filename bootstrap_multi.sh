f361c88fb599715e2410f6880112700d29ac6774
IdMineThat <100437916+IdMineThat@users.noreply.github.com>
2022-10-29 19:51:19 -0500

    Update bootstrap_multi.sh

--- bootstrap_multi.sh
+++ bootstrap_multi.sh
@@ -2,15 +2,15 @@
 
 ################################################################################
 # Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox #
-# Updated by Dirty Harry for YiiMP use...                                      #
+# Updated by IdMineThat for YiiMP use...                                      #
 # This script is intended to be ran from the Yiimp Server Installer            #
 ################################################################################
 
 # Clone the MultiPool repository if it doesn't exist.
 if [ ! -d $HOME/yiimpserver/yiimp_multi ]; then
-	echo Downloading Dirty Harry Yiimp Multi Server Installer V1.0. . .
+	echo Downloading IdMineThat Yiimp Multi Server Installer V1.0. . .
 	git clone \
-		https://github.com/DirtyHarryDev/server_yiimp_multi \
+		https://github.com/IdMineThat/server_yiimp_multi \
 		$HOME/yiimpserver/yiimp_multi \
 		< /dev/null 2> /dev/null
 
