0cff5bac0ed69ee7197ffdc8e0f706784a04e89c
IdMineThat <100437916+IdMineThat@users.noreply.github.com>
2022-10-29 20:13:59 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,12 +1,12 @@
 ################################################################################
 # Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox #
-# Updated by Dirty Harry for YiiMP use...                                      #
+# Updated by IdMineThat for YiiMP use...                                      #
 # This script is intended to be ran from the Yiimp Server Installer            #
 ################################################################################
 
 source /etc/functions.sh
 
-RESULT=$(dialog --stdout --nocancel --default-item 1 --title "Dirty Harry Yiimp Server Installer v1.0" --menu "Choose one" -1 60 16 \
+RESULT=$(dialog --stdout --nocancel --default-item 1 --title "IdMineThat Yiimp Server Installer v1.0" --menu "Choose one" -1 60 16 \
 ' ' "- YiiMP Server Install -" \
 1 "YiiMP Single Server" \
 ' ' "- More Will Be Added Later -" \
